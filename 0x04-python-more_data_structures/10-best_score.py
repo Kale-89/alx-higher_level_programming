e7f9d4784f13649538238e80025cfe7c3b86dcc6
Kalehiwot <kalehiwotm@gmail.com>
2022-07-08 15:27:05 +0300

    Mandatory Task update

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
 
-def multiply_by_2(a_dictionary):
-    return ({k: a_dictionary[k] * 2 for k in a_dictionary})
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value."""
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+
+    ret = list(a_dictionary.keys())[0]
+    big = a_dictionary[ret]
+    for k, v in a_dictionary.items():
+        if v > big:
+            big = v
+            ret = k
+    return (ret)
