4338d195de9a2d1bb47aebd4a622e436fc596ac4
Kalehiwot <kalehiwotm@gmail.com>
2022-07-18 11:05:14 +0300

    Mandatory Task commit

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,12 +4,12 @@ BUZZ = "Buzz"
 
 
 def fizzbuzz():
-    for number in range(1, 101):
-        if (number % 3 and number % 5):
-            print("%s%s" % (FIZZ, BUZZ), end=' ')
-        elif (number % 3):
+    for number in range(1, 101)
+        if (number % 3):
             print("%s" % (FIZZ), end=' ')
         elif (number % 5):
             print("%s" % (BUZZ), end=' ')
+        elif (number % 3 and number % 5):
+            print("%s%s" % (FIZZ BUZZ), end=' ')
         else:
             print("%d" % (number), end=' ')
