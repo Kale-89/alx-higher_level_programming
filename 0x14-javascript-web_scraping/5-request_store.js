547f28062b628c0fee3569b41b6a04bb5fed308e
Kalehiwot <kalehiwotm@gmail.com>
2022-11-15 22:22:32 +0300

    fixs

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 
-const request = require('request');
 const fs = require('fs');
-const url = process.argv[2];
-const filePath = process.argv[3];
+const request = require('request');
 
-request(url, function (err, response, body) {
+request(process.argv[2], (err, res, body) => {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    fs.writeFile(process.argv[3], body, 'utf8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
   }
 });
